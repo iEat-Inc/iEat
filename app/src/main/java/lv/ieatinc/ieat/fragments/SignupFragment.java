bc006b12696c5e77e186bd1cd23960a48505c57c
AlinaVerkholomova <alina.verkholomova@gmail.com>
2022-07-13 10:19:21 +0300

    Implemented back arrow in Signup page

--- app/src/main/java/lv/ieatinc/ieat/fragments/SignupFragment.java
+++ app/src/main/java/lv/ieatinc/ieat/fragments/SignupFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import lv.ieatinc.ieat.R;
 
@@ -21,5 +22,14 @@ public class SignupFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         Log.i(TAG, "START");
+
+        ImageView back_arrow = view.findViewById(R.id.signup_back_arrow);
+        back_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Does the same as Back button, goes back to previous fragment, destroying current
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
     }
 }
