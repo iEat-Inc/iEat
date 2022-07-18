5a6929e4e349559f37e28fbc2debf03557261f79
AlinaVerkholomova <alina.verkholomova@gmail.com>
2022-07-18 15:16:43 +0300

    New Shelf page UI created

---
+++ app/src/main/java/lv/ieatinc/ieat/NewShelfActivity.java
@@ -0,0 +1,19 @@
+package lv.ieatinc.ieat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class NewShelfActivity extends AppCompatActivity {
+
+    ImageView backArrow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_shelf);
+        backArrow = findViewById(R.id.new_shelf_back_arrow);
+        backArrow.setOnClickListener(v -> finish());
+    }
+}
